a797a7ce2d6b96c89237fc432fdbd728750475ca
Tito Dakshinamoorthy <titodakshinamoorthy@hotmail.com>
2016-03-28 23:21:35 +0530

    Simple binding sample

---
+++ Angular1Sample/scripts/custom/simplebinding.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../angular.min.js" />
+var app = angular
+    .module("sbmodule", [])
+    .controller("sbcontroller", function ($scope) {
+        var employee = {
+            firstName: "emp1 first name",
+            lastName: "emp1 last name",
+            gender: "Male"
+        };
+
+        $scope.employee = employee;
+    });
